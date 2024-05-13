f44ec132890864f0dfcb757f1751cfb7469a1331
Kerneld82 <kerneld@naver.com>
2024-05-13 21:23:06 +0900

    feat: 홈 화면 수정

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,64 @@
 import json
 
 import dash_bootstrap_components as dbc
-from dash import html, ctx, Input, Output, State, clientside_callback, dcc
+from dash import html, ctx, Input, Output, State, clientside_callback, dcc, dash_table
+import pandas as pd
+import plotly.express as px
+from sklearn import preprocessing
+import plotly.io as pio
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+from plotly.validators.scatter.marker import SymbolValidator
+
+dfSellIdx = pd.read_csv('data/Data.csv')
+dfSellIdx = dfSellIdx[dfSellIdx['기준년도'] >= 2018]
+
+# 정부 정책
+dfGov = pd.read_csv('data/정부_부동산_대책.csv')
+
+dfMerged = pd.merge(left = dfSellIdx, right = dfGov, on = ['기준년도', '기준월'], how = 'left')
+
+# 연도-월 컬럼 추가
+ymList = list(map(lambda x: '%d-%02d' % (int(dfMerged['기준년도'][x]), int(dfMerged['기준월'][x])), range(len(dfMerged))))
+dfMerged['ym'] = pd.DataFrame(ymList)
+
+#fig = px.scatter(dfMerged, x='ym', y='아파트매매실거래가격지수', text='약칭', line_shape='linear')
+#fig.update_traces(textposition='top center')
+
+#fig = go.Figure()
+#fig.add_trace(go.Scatter(x=dfMerged['ym'], y=dfMerged['아파트매매실거래가격지수'], mode='lines+markers+text', 
+#                         name='아파트매매실거래가격지수',
+#                         text=dfMerged['약칭'],
+#                         textposition="top center"))
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=dfMerged['ym'], y=dfMerged['아파트매매실거래가격지수'], mode='lines+markers+text', 
+                         name='아파트매매실거래가격지수'))
+fig.update_layout(showlegend=True, height=600)
+
+dfForAnnotation = dfMerged
+dfForAnnotation.dropna(axis=0, inplace=True)
+
+for i in range(len(dfForAnnotation)):
+    fig.add_annotation(x=dfForAnnotation.iloc[i]['ym'], y=dfForAnnotation.iloc[i]['아파트매매실거래가격지수'], 
+                    text=dfForAnnotation.iloc[i]['약칭'], showarrow=True,
+                    font=dict(family='Courier New, monospace',
+                                size=13,
+                                color='blue'),
+                    align='center',
+                    arrowhead=2,
+                    arrowsize=1,
+                    arrowwidth=2,
+                    arrowcolor='orange',
+                    ax=dfForAnnotation.iloc[i]['annot_x'], ay=dfForAnnotation.iloc[i]['annot_y'],
+                    bordercolor='red',
+                    borderwidth=2,
+                    borderpad=4,
+                    bgcolor='yellow',
+                    opacity=0.8)
+
 
 def get_sidebar(active_item=None):
     nav = html.Nav(id="sidebar", className="active", children=[
@@ -38,14 +95,11 @@ def layout():
         ]),
     ])
 
-    layout_components = dbc.Row([
-        dbc.Col([
-            html.Div(html.P('You can zoom, rotate the virus model in the 3D Viewer to explore the virus.'), className='text-justify'),
-            html.Div(html.P('You can zoom, rotate the virus model in the 3D Viewer to explore the virus.'), className='text-justify'),
-            html.Div(html.P('You can zoom, rotate the virus model in the 3D Viewer to explore the virus.'), className='text-justify'),
-            html.Div(html.P('You can zoom, rotate the virus model in the 3D Viewer to explore the virus.'), className='text-justify'),
-        ]),
-    ], className='mb-4 mt-2 align-items-end')
+    layout_components = html.Div([
+        #html.Div(children='My First App with Data and a Graph'),
+        #dash_table.DataTable(data=dfMerged.to_dict('records'), page_size=10),
+        dcc.Graph(figure=fig),
+    ])
 
     layout = [
         get_sidebar(__name__),
